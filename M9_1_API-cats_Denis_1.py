6558e3a153d8fa82962d1c4ee1c8cf68f3531bb5
SkyyNikole <elanan7724@gmail.com>
2024-10-15 13:18:54 +0300

    def load_image() function - to get images

--- M9_1_API-cats_Denis_1.py
+++ M9_1_API-cats_Denis_1.py
@@ -3,6 +3,24 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image():
+    try:
+        response = requests.get(url)
+#_делаем запрос по url и то, что вернется, положим в response
+        response.raise_for_status()
+#_для обработки исключений, если будет ошибка, мы ее получим здесь
+        image_data = BytesIO(response.content)
+#_обрабатываем полученное изображение в байтах
+        image_got = Image.open(image_data)
+#_полученное изображение открываем с помощью pillow
+        return ImageTk.PhotoImage(image_got)
+#_функция возвращает нам полученное изображение, которое мы положим
+#_new_image = load_image(url), дальше положим label.config(image = new_image)
+    except Exception as e:
+        print(f'Произошла ошибка: {e}.')
+        return None
+
 window = Tk()
 window.title('Kitties_Tomcats_Kittens')
 window.geometry('600x480')
@@ -16,7 +34,7 @@ new_image = load_image(url)
 # будет возвращаться картинка, которую поместим в label)
 
 if new_image:
-    label.config(image=new_image)
+    label.config(image = new_image)
     label.image = new_image
 #_после получения new_image, проверяем, что new_image есть,
 #_и выводим на метку, изменяя атрибут метки - image
