46cffdd2e19b52a3d8b8a3b0d28e9a03decb6234
SkyyNikole <elanan7724@gmail.com>
2024-10-15 15:54:02 +0300

    new file - for project toplevel window added

---
+++ M_9_1_API_cats_Denis_2.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+        return None
+
+def open_new_window():
+    img = load_image(url)
+    if img:
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
+        label.image = img  # Сохраняем ссылку на изображение
+
+
+def exit():
+    window.destroy()
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x520")
+
+# Создаем меню
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+# Добавляем пункты меню
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
+
+url = 'https://cataas.com/cat/cute'
+
+
+window.mainloop()
+
