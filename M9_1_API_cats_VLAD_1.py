cb15c3f4578ed96604a4e9e4aed4c85dd23b7a98
SkyyNikole <elanan7724@gmail.com>
2024-10-16 09:50:31 +0300

    def get_cat(url)

--- M9_1_API_cats_VLAD_1.py
+++ M9_1_API_cats_VLAD_1.py
@@ -3,6 +3,19 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
+#_4 функция получаем картинку, вернули через return и запомнилась она в img (#_3)
+def get_cat(url):
+    get_bytes_img = requests.get(url)
+#_4.1_отправляем запрос на сайт url и получаем ответ в виде картинки в битовом формате
+    norm_img = BytesIO(get_bytes_img.content)
+#_4.2_преобразовали и битового форм контент в норм картинку
+    norm_img_opened = Image.open(norm_img)
+#_4.3_открываем с помощью библ PIL
+    img_tk = ImageTk.PhotoImage(norm_img_opened)
+#_4.4_преобразуем для tkinter
+    return img_tk
+#_4.5_возвращаем на функцию
+
 
 window = Tk()
 window.title('Cats and Kittens')
@@ -12,12 +25,10 @@ window.iconbitmap('black_cat.ico')
 url = 'https://cataas.com/cat'
 #2_переменная со ссылкой на image с сайта
 img = get_cat(url)
-#3_в переменную img помещаяем функцию с параметром url, который она получит
+#3_в переменную img помещаяем функцию с параметром url, на который она получит картинку с сайта
 
-label = Label()
+label = Label(window, image = img)
 #1_закинем сюда image c сайта
 label.pack()
-
-
 
 window.mainloop()
