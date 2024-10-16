38c907a254c322692a083a232351bb01aa2c3e3a
SkyyNikole <elanan7724@gmail.com>
2024-10-16 09:19:48 +0300

    window.iconbitmap('black_cat.ico') added

--- M9_1_API_cats_VLAD_1.py
+++ M9_1_API_cats_VLAD_1.py
@@ -4,8 +4,8 @@ from PIL import Image, ImageTk
 from io import BytesIO
 
 window = Tk()
-window.title('Cats and Kitties')
+window.title('Cats and Kittens')
 window.geometry(f'500x400+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}')
-
+window.iconbitmap('black_cat.ico')
 
 window.mainloop()
