682529f7047c87592a2e449c6f80e398d5654d01
SkyyNikole <elanan7724@gmail.com>
2024-10-16 09:25:53 +0300

    url = 'https://cataas.com/cat' #2_переменная со ссылкой на image с сайта img = get_cat(url) #3_в переменную img помещаяем функцию с параметром url, который она получит
    label = Label()
    #1_закинем сюда image c сайта

--- M9_1_API_cats_VLAD_1.py
+++ M9_1_API_cats_VLAD_1.py
@@ -3,9 +3,21 @@ import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
+
 window = Tk()
 window.title('Cats and Kittens')
 window.geometry(f'500x400+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}')
 window.iconbitmap('black_cat.ico')
+
+url = 'https://cataas.com/cat'
+#2_переменная со ссылкой на image с сайта
+img = get_cat(url)
+#3_в переменную img помещаяем функцию с параметром url, который она получит
+
+label = Label()
+#1_закинем сюда image c сайта
+label.pack()
+
+
 
 window.mainloop()
