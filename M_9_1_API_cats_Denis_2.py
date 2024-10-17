6ec825ee63cc003aeacf2f576cdc59a33667089b
SkyyNikole <elanan7724@gmail.com>
2024-10-17 08:38:07 +0300

    combobox added (list of tags for combobox) entry deleted button replaced (under combobox)

--- M_9_1_API_cats_Denis_2.py
+++ M_9_1_API_cats_Denis_2.py
@@ -1,8 +1,12 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+Allowed_tags = ['psychedelic', 'sleep', 'black', 'fight', 'jump', 'lazy', 'newyear', 'poptart', 'relax', 'programmer']
+#_создаем список для Combobox
 
 def load_image(url):
     try:
@@ -17,14 +21,15 @@ def load_image(url):
         return None
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
+#_берем из combobox - вместо tag_entry.get()
     url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
     img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title('Картинка с котиком')
         new_window.geometry('600x480')
-        label = Label(new_window, image=img)
+        label = Label(new_window, image = img)
         label.pack()
         label.image = img  # Сохраняем ссылку на изображение
 
@@ -36,11 +41,9 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
-tag_entry = Entry(window)
-tag_entry.pack()
-
-load_btn = Button(window, text='Загрузить по тегу', command = open_new_window)
-load_btn.pack()
+#_tag_entry = Entry(window)
+#_tag_entry.pack()
+#_после создания Combobox - убираем
 
 # Создаем меню
 menu_bar = Menu(window)
@@ -55,6 +58,13 @@ file_menu.add_command(label="Выход", command=exit)
 
 url = 'https://cataas.com/cat'
 
+tag_label = Label(window, text = 'Выбери tag')
+tag_label.pack()
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
 
+load_btn = Button(window, text='Загрузить по тегу', command = open_new_window)
+load_btn.pack()
+#_переместили кнопку под Combobox (была под entry)
 window.mainloop()
 
